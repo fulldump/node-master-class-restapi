099add7d2231b83e09c19b384cb377643233bbfd
fulldump <gerardooscarjt@gmail.com>
2019-01-02 00:54:05 +0100

    feat: Parsing request paths

--- index.js
+++ index.js
@@ -4,10 +4,22 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should responde to all requests with a string
 const server = http.createServer(function(req, res) {
+
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path from the url
+  const path = parsedUrl.pathname;
+
+  // Send the response
   res.end('Hello World\n');
+
+  // Log path
+  console.log(new Date(), path);
 });
 
 // Start the server, and have it listen on port 3000
