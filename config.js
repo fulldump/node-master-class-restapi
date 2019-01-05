eac24623f46a4a3b7847194637928b1a2199d597
fulldump <gerardooscarjt@gmail.com>
2019-01-05 18:00:03 +0100

    feat: extend config to support twilio info

--- config.js
+++ config.js
@@ -18,6 +18,13 @@ environments.staging = {
   hashingSecret: '4e813ba4-0ef2-11e9-b82a-3f52039d5202',
 
   maxChecks: 5,
+
+  twilio: {
+    fromPhone: '',
+    accountSid: '',
+    authToken: '',
+  },
+
 };
 
 // Production
@@ -33,6 +40,13 @@ environments.production = {
   hashingSecret: '529ce9ea-0ef2-11e9-8020-9b2eaffb007a',
 
   maxChecks: 5,
+
+  twilio: {
+    fromPhone: '',
+    accountSid: '',
+    authToken: '',
+  },
+
 };
 
 // Determine which environment was passed as a command-line argument
