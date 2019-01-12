f819ef521429197a3896b3cb6ea9bd47d6112a02
fulldump <gerardooscarjt@gmail.com>
2019-01-12 12:15:14 +0100

    feat: use new injectable router instead of the static one
    * Working code :D

--- index.js
+++ index.js
@@ -2,16 +2,26 @@
  * Primary file for the API
  */
 
- // Dependencies
- const server = require('./lib/server');
-
+// Dependencies
+const server = require('./lib/server');
+const handlers = require('./lib/handlers');
 // Declare the app
 const app = {};
 
 // Init function
 app.init = function() {
 
+  // Instantiate a new server
   var s = new server.Server();
+
+  // Set some handlers
+  s.router.path('/sample')
+    .method('GET', handlers.sample);
+
+  s.router.path('/ping')
+    .method('*', handlers.ping);
+
+  // Start listenint
   s.start();
 
 };
