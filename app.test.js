72d9624521a7cbd21a8843cae4a1ddb18a8fc4d9
fulldump <gerardooscarjt@gmail.com>
2019-01-13 22:43:56 +0100

    refactor: move makeRequest from app.test.js to helpers.js

--- app.test.js
+++ app.test.js
@@ -9,66 +9,12 @@ const https = require('https');
 const http = require('http');
 const url = require('url');
 const helpers = require('./lib/helpers');
+const makeRequest = helpers.makeRequest;
 const datalib = require('./lib/data');
 
 // Initialize suite
 var suite = new testing.Suite();
 
-// Testing helpers
-function makeRequest(method, uri, headers, body) {
-
-  headers = headers || {};
-  body = body || '';
-  if (!(typeof body == 'string')) {
-    body = JSON.stringify(body);
-  }
-
-  return new Promise(function(resolve, reject) {
-    const parsedUrl = url.parse(uri);
-
-    const requestDetails = {
-      protocol: parsedUrl.protocol,
-      hostname: parsedUrl.hostname,
-      port: parsedUrl.port,
-      method,
-      headers,
-      path: parsedUrl.path,
-      timeout: 2 * 1000, // @TODO: hardcoded value
-    };
-
-
-    var client = 'https:' === parsedUrl.protocol ? https : http;
-    var req = client.request(requestDetails, function(res) {
-      // Configure encoding
-      res.setEncoding('utf8');
-
-      // Collect body
-      var payload = '';
-      res.on('data', function(chunk) {
-        payload += chunk;
-      })
-      res.on('end', function() {
-        try {
-          payload = JSON.parse(payload);
-        } catch (e) {
-
-        }
-        res.payload = payload;
-        resolve(res);
-      });
-    });
-
-    // Bind to the error event so it does not ge thrown
-    req.on('error', reject);
-
-    // Bind to the timeout event
-    req.on('timeout', reject);
-
-    // End the request
-    req.end(body, 'utf8');
-
-  });
-}
 
 // Write all tests cases
 
